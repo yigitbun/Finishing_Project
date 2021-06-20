3a85cde530998e30969e67f2d02ffc9bdbfce5e5
Bünyamin Yigit <yigitbun@gmail.com>
2021-06-20 21:04:03 +0200

    update

---
+++ Flask_Web/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import TeamITea
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    result = TeamITea.vorhersage('gut')
+    return render_template("index.html",result=result)
+
+@app.route("/about")
+def about():
+    return "About"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
